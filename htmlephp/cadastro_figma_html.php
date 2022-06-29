2e96b073134a590da6e14769ec32a23485000cb5
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2022-06-29 16:25:50 -0300

    Moving js's into js folder

--- htmlephp/cadastro_figma_html.php
+++ htmlephp/cadastro_figma_html.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,28 +19,8 @@
     <link rel="icon" type="image/x-icon" href="../imgs/logoempresa.png">
 
 </head>
-<body>
-    <!-- Início barra entalhe no topo em todas as telas -->
-    <div id="entalhe">
-        <div id="hora">
-            <thead>
-                <tr>
-                    <th>12:52</th>
-                </tr>
-            </thead>
-        </div>
-        <div class="iconsignal">
-            <img src="../imgs/signal.png" alt="Sinal" width="70px" height="70px">
-        </div>
-        <div class="iconwifi">
-            <img src="../imgs/wifi.png" alt="Wifi" width="60px" height="60px">
-        </div>
-        <div class="iconbattery">
-            <img src="../imgs/battery.png" alt="Bateria" width="70px" height="70px">
-        </div>
-    </div>
 
-    <!-- Término da barra entalhe no topo em todas as telas -->
+<body>
 
     <div id="subentalhe">
         <p class="subtitulo">Pizzaria IQ</p>
@@ -51,49 +32,50 @@
 
     <div id="fundo_cadastro">
 
-      <form action="cadastrar.php" method="post">
-        <div class="cadastro_body">
-            <div id="name_struct">
-                <img src="../imgs/icon_name.png" class="icon_name">  
-                <input type="text" name="nome_usuario" id="nome_usuario" placeholder="NOME:"><br><br>
+        <form action="cadastrar.php" method="post">
+            <div class="cadastro_body">
+                <div id="name_struct">
+                    <img src="../imgs/icon_name.png" class="icon_name">
+                    <input type="text" name="nome_usuario" id="nome_usuario" placeholder="NOME:"><br><br>
+                </div>
+                <div id="fone_struct">
+                    <img src="../imgs/icon_fone.png" class="icon_fone">
+                    <input type="text" name="telefone_usuario" id="telefone_usuario" placeholder="TELEFONE:"><br><br>
+                    <small id="textTelefone"></small>
+                </div>
+                <div id="email_struct">
+                    <img src="../imgs/icon_email.png" class="icon_email">
+                    <input type="text" name="email_usuario" id="email_usuario" placeholder="EMAIL:"><br><br>
+                    <small id="textEmail"></small>
+                </div>
+                <div id="senha_struct">
+                    <img src="../imgs/icon_senha.png" class="icon_senha">
+                    <input type="text" name="senha_usuario" id="senha_usuario" placeholder="SENHA:"><br><br>
+                    <small id="textSenha"></small>
+                </div>
+                <div id="endereço_struct">
+                    <img src="../imgs/icon_endereço.png" class="icon_endereço">
+                    <input type="text" name="endereco_usuario" id="endereco_usuario" placeholder="ENDEREÇO:"><br><br>
+                </div>
             </div>
-            <div id="fone_struct">
-                <img src="../imgs/icon_fone.png" class="icon_fone">  
-                <input type="text" name="telefone_usuario" id="telefone_usuario" placeholder="TELEFONE:"><br><br>
-                <small id="textTelefone"></small>
-            </div>
-            <div id="email_struct">
-                <img src="../imgs/icon_email.png" class="icon_email">  
-                <input type="text" name="email_usuario" id="email_usuario" placeholder="EMAIL:"><br><br>
-                <small id="textEmail"></small>
-            </div>
-            <div id="senha_struct">
-                <img src="../imgs/icon_senha.png" class="icon_senha">  
-                <input type="text" name="senha_usuario" id="senha_usuario" placeholder="SENHA:"><br><br>
-                <small id="textSenha"></small>
-            </div>
-            <div id="endereço_struct">
-                <img src="../imgs/icon_endereço.png" class="icon_endereço">   
-                <input type="text" name="endereco_usuario" id="endereco_usuario" placeholder="ENDEREÇO:"><br><br>
-            </div>
-        </div>
 
             <div id="botao_register">
                 <button id="register" type="submit" name="cadastrar" value="Cadastrar">CADASTRAR</button>
             </div>
 
-      </form>
-      <div>
-        <button id="nadaver" onclick="location.href='login_figma_html.php'">VOLTAR AO LOGIN</button><br><br>
-      </div>
+        </form>
+        <div>
+            <button id="nadaver" onclick="location.href='login_figma_html.php'">VOLTAR AO LOGIN</button><br><br>
+        </div>
     </div>
 
 
 
-        <div class="help">Precisa de ajuda?</div>
+    <div class="help">Precisa de ajuda?</div>
 
     <!-- Término botões e textos da tela login -->
 
-    <script src="cadastro_figma.js"></script>
+    <script src="../js/cadastro_figma.js"></script>
 </body>
+
 </html>
--- htmlephp/login_figma_html.php
+++ htmlephp/login_figma_html.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,28 +19,8 @@
     <link rel="icon" type="image/x-icon" href="../imgs/logoempresa.png">
 
 </head>
-<body>
-    <!-- Início barra entalhe no topo em todas as telas -->
-    <div id="entalhe">
-        <div id="hora">
-            <thead>
-                <tr>
-                    <th>12:52</th>
-                </tr>  
-            </thead>
-        </div>
-        <div class="iconsignal">
-            <img src="../imgs/signal.png" alt="Sinal" width="70px" height="70px">
-        </div>
-        <div class="iconwifi">
-            <img src="../imgs/wifi.png" alt="Wifi" width="60px" height="60px">
-        </div>
-        <div class="iconbattery">
-            <img src="../imgs/battery.png" alt="Bateria" width="70px" height="70px">
-        </div>
-    </div>
 
-    <!-- Término da barra entalhe no topo em todas as telas -->
+<body>
 
     <!-- Início do fundo da tela login  -->
 
@@ -55,39 +36,40 @@
 
 
     <div class="text_1_login"> Sua pizza está quase pronta!</div>
-    
+
     <div id="fundo_login_login">
 
         <form action="logar.php" method="post">
             <div class="cadastro_body_login">
                 <div id="email_struct_login">
-                    <img src="../imgs/icon_email.png" class="icon_email">  
+                    <img src="../imgs/icon_email.png" class="icon_email">
                     <input type="text" name="email_usuario_login" id="email_usuario_login" placeholder="EMAIL:"><br><br>
                     <small id="textEmail_login"></small>
-                 </div>
-                 <div id="senha_struct_login">  
-                    <img src="../imgs/icon_senha.png" class="icon_senha">  
+                </div>
+                <div id="senha_struct_login">
+                    <img src="../imgs/icon_senha.png" class="icon_senha">
                     <input type="text" name="senha_usuario_login" id="senha_usuario_login" placeholder="SENHA:"><br><br>
                     <small id="textSenha_login"></small>
-                 </div>
-            </div>
-  
-                <div id="botao_register_login">
-                    <button id="register_login" type="submit" name="logar" value="Logar">LOGAR</button>
                 </div>
-  
+            </div>
+
+            <div id="botao_register_login">
+                <button id="register_login" type="submit" name="logar" value="Logar">LOGAR</button>
+            </div>
+
         </form>
 
         <div id="botao_nadaver">
             <button id="nadaver" onclick="location.href='cadastro_figma_html.php';">CADASTRAR</button>
         </div>
 
-      </div>
+    </div>
 
     <div class="help">Precisa de ajuda?</div>
 
     <!-- Término botões e textos da tela login -->
 
-    <script src="login_figma.js"></script>
+    <script src="../js/login_figma.js"></script>
 </body>
+
 </html>
